4b5f33d9f6fdc7f5155f839c7de58f35b7de974f
tomasmedici <tomas@medici.dk>
2018-09-30 12:35:09 +0200

    Initial commit

---
+++ wordWiz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wordWiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

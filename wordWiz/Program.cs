8b879246ed3b67030d192aa9fdf159cd8781690e
tomasmedici <tomas@medici.dk>
2018-09-30 13:22:47 +0200

    Third commit for testing

--- wordWiz/Program.cs
+++ wordWiz/Program.cs
@@ -6,7 +6,8 @@ namespace wordWiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, is this my world! But are you sure? Yes I am!");
+            Console.WriteLine("Hello, is this my world! But are you sure? Yes I am! This is also at the line....");
+            Console.WriteLine("This is a new line, I hope");
         }
     }
 }
